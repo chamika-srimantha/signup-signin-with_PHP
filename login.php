8b36770f5d971b5f9921387f0f6b959a099b35ec
Chamika Srimantha <84226079+chamika-srimantha@users.noreply.github.com>
2023-01-21 18:28:08 +0530

    Add files via upload

---
+++ login.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8"/>
+    <title>Login</title>
+    <link rel="stylesheet" href="style.css"/>
+</head>
+<body>
+
+<header>
+    <h2>Chamika</h2>
+    <nav>
+      <a class="a1" href="#">HOME</a>
+      <a class="a1" href="#">BLOG</a>
+      <a class="a1" href="#">CONTACT</a>
+      <a class="a1" href="#">ABOUT</a>
+    </nav>
+    <div class='sign-in-up'>
+      <a class="hello" href="login.php">LOGIN</a>
+      <a class="hello" href="registration.php">REGISTER</a>
+    </div>
+</header>
+
+<?php
+    require('db.php');
+    session_start();
+    // When form submitted, check and create user session.
+    if (isset($_POST['username'])) {
+        $username = stripslashes($_REQUEST['username']);    // removes backslashes
+        $username = mysqli_real_escape_string($con, $username);
+        $password = stripslashes($_REQUEST['password']);
+        $password = mysqli_real_escape_string($con, $password);
+        // Check user is exist in the database
+        $query    = "SELECT * FROM `users` WHERE username='$username'
+                     AND password='$password'";
+        $result = mysqli_query($con, $query) or die(mysql_error());
+        $rows = mysqli_num_rows($result);
+        if ($rows == 1) {
+            $_SESSION['username'] = $username;
+            // Redirect to user dashboard page
+            header("Location: dashboard.php");
+        } else {
+            echo "<div class='form'>
+                  <h3>Incorrect Username/password.</h3><br/>
+                  <p class='link'>Click here to <a href='login.php'>Login</a> again.</p>
+                  </div>";
+        }
+    } else {
+?>
+    <form class="form" method="post" name="login">
+        <h1 class="login-title">Login</h1>
+        <input type="text" class="login-input" name="username" placeholder="Username" autofocus="true"/>
+        <input type="password" class="login-input" name="password" placeholder="Password"/>
+        <input type="submit" value="Login" name="submit" class="login-button"/>
+        <p class="link"><a href="registration.php">New Registration</a></p>
+  </form>
+<?php
+    }
+?>
+</body>
+</html>
